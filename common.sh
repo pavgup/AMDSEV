5758cb6b1b3973ef60ecc3beea9a7d7706ed9d65
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2023-06-26 14:50:46 +0200

    Use Azure config for host kernel

--- common.sh
+++ common.sh
@@ -70,7 +70,7 @@ build_kernel()
 				run_cmd ./scripts/config --enable VIRT_DRIVERS
 				run_cmd ./scripts/config --enable SEV_GUEST
 			else
-				run_cmd "cp /boot/config-$(uname -r) .config"
+				cp ../../host-config .config
 				run_cmd ./scripts/config --module  SEV_GUEST
 			fi
 			run_cmd ./scripts/config --set-str LOCALVERSION "$VER-$COMMIT"
