f091e2eff9d1d757359c35964b0c2d5b65eeeb96
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2023-10-12 11:38:34 +0200

    repack: Fix sed args to prevent creating '*.she' files
    Signed-off-by: Jeremi Piotrowski <jpiotrowski@microsoft.com>

--- repack.sh
+++ repack.sh
@@ -49,7 +49,7 @@ override_dh_auto_install:
 	tar -xpzf snp-release.tar.gz --strip-components=1 -C debian/${PACKAGE_NAME}
 	cd debian/${PACKAGE_NAME} && mv launch-qemu.sh usr/local/bin/
 	cd debian/${PACKAGE_NAME} && rm -rf kvm.conf install.sh linux
-	cd debian/${PACKAGE_NAME}/usr/local/bin && sed -ie 's|^EXEC_PATH.*|EXEC_PATH="\$\$(readlink -f "\$\$(dirname "\$\$0")/..")"|' launch-qemu.sh
+	cd debian/${PACKAGE_NAME}/usr/local/bin && sed -i -e 's|^EXEC_PATH.*|EXEC_PATH="\$\$(readlink -f "\$\$(dirname "\$\$0")/..")"|' launch-qemu.sh
 	install -d -m 755 debian/${PACKAGE_NAME}/usr/local/share/${PACKAGE_NAME}/
 	install -m 644 -t debian/${PACKAGE_NAME}/usr/local/share/${PACKAGE_NAME}/ debian-11-nocloud-amd64.qcow2
 	install -m 644 -t debian/${PACKAGE_NAME}/usr/local/share/${PACKAGE_NAME}/ vmlinuz* 
