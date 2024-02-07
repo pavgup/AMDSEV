e8fe4c70a80a01eadaf82f9a2aeb1453f325144e
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2024-02-07 11:28:46 +0100

    run: Switch to Ubuntu 22.04 for everything

--- run.sh
+++ run.sh
@@ -2,18 +2,14 @@
 
 set -ex
 
-docker build -t amdsev-build:default docker/
 docker build --build-arg UBUNTU_VERSION=22.04 -t amdsev-build:22.04 docker/
 
-run() {
-  docker run --init --rm -v $PWD/.ccache:/root/.ccache -v $PWD:$PWD -w $PWD amdsev-build:default "$@"
-}
 run2204() {
   docker run --init --rm -v $PWD/.ccache:/root/.ccache -v $PWD:$PWD -w $PWD amdsev-build:22.04 "$@"
 }
 
 run2204 ./build.sh qemu
 run2204 ./build.sh ovmf
-run ./build.sh --package kernel guest
+run2204 ./build.sh --package kernel guest
 
 run2204 ./repack.sh
